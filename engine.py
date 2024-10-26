216b7315dbd1c3c5c29c8acb9e735a82b8875f82
Noob <53237138+m1kr0ch1p@users.noreply.github.com>
2024-10-25 22:00:06 -0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,27 +1,20 @@
 import os
 import asyncio
 import details as ds
-import requests
 from telethon import TelegramClient, types, sync
 from telethon.tl.functions.messages import GetHistoryRequest
 from colorama import Fore, Style
-from datetime import datetime
 
-
-# variaveis de usuario para gerar a sessao do Telegram
+# User's variable to create Telegram session
 api_id = ds.apiID
 api_hash = ds.apiHash
 phone = ds.number
 client = TelegramClient(phone, api_id, api_hash)
-channel_list = []
-channel_list.append(input(str('Give me the Channel URL:\n')))
 
-# channel_list = [-10000000000000]
-
-# Faz o download de arquivos postados
+# Download posted files
 async def content_downloader(channel_name, output_directory):
     async with client:
-        # Recebe o nome do canal
+        # Get channel name
         channel = await client.get_entity(channel_name)
         offset_id = 0
         limit = 100
@@ -29,14 +22,13 @@ async def content_downloader(channel_name, output_directory):
         total_messages = 0
 
         while True:
-            # Coleta o history do canal
+            # Collect channel history
             history = await client(GetHistoryRequest(
                 peer=channel,
                 offset_id=offset_id, offset_date=None,
                 add_offset=0, limit=limit,
                 max_id=0, min_id=0, hash=0))
             messages = history.messages
-
 
             for message in messages:
                 if message.document:
@@ -50,16 +42,19 @@ async def content_downloader(channel_name, output_directory):
                     if not file_name:
                         file_name = f'document_{message.id}'
 
-                    # Verifica a extensão do arquivo
+                    # Verify files extension
                     if file_name.lower().endswith(('.txt', '.zip', '.rar', '.jpg', '.png', '.pdf')):
-                        file_path = os.path.join(output_directory, file_name)
+                        downloaded_files = f'{output_directory}/downloaded_files'
+                        if not os.path.exists(output_directory):
+                            os.makedirs(output_directory)
 
-                        # Verifica se o arquivo já foi baixado e realiza o download
+                        file_path = os.path.join(downloaded_files, file_name)
+                        # Check if files were previously downloaded
                         if not os.path.exists(file_path):
                             await client.download_media(message, file_path)
-                            print(f'{file_name} downloaded in {output_directory}.')
+                            print(f'[+] {file_name} downloaded in {downloaded_files}.')
                         else:
-                            print(f'{file_name} already exists.')
+                            print(f'[!] {file_name} already exists.')
 
             all_messages.extend(messages)
             if len(messages) < limit:
@@ -68,7 +63,7 @@ async def content_downloader(channel_name, output_directory):
             offset_id = messages[-1].id
             total_messages = len(all_messages)
 
-# Faz o scrape no canal e coleta os dados
+# Scrapping channel and collecting data
 async def scrape_channel_content(channel_name):
     async with client:
         try:
@@ -107,24 +102,21 @@ async def scrape_channel_content(channel_name):
             print(f"An error occurred: {Fore.RED}{e}{Style.RESET_ALL}")
             return []
 
-
-# Recebe a URL do canal, define os nomes de diretorio e de arquivo e os dados coletados das mensagens
+# Gets channel URL, set collected data, directory and file names 
 async def collect():
-    import csv
     import pandas as pd
     from analytics import analyse
+    from datetime import datetime
 
     for channel_name in channel_list:
         try:
             today = datetime.now().strftime("%Y-%m-%d")
-            # Descomente caso queira definir os canais por IDs
-            #channel_name_filtered = str(channel_name).replace("-","")
 
-            # Descomente caso queira definir os canais por links
+            # Gives channel name to directory
             channel_name_filtered = channel_name.rsplit("/",1)[-1]
-
-            # Define diretorio de saida
+            # Defines output for files store
             output_directory = f'CaseFiles/{channel_name_filtered}'
+
             # Verifica se o diretorio ja existe / nomeia o diretorio
             if not os.path.exists(output_directory):
                 os.makedirs(output_directory)
@@ -134,7 +126,6 @@ async def collect():
             print(f'[+] Scraping content from {Fore.LIGHTYELLOW_EX}{channel_name}{Style.RESET_ALL}...')
 
             # Executa a funcao scrape_channel_content com o canal indicado
-
             content = await scrape_channel_content(channel_name)
 
             # Armazena os dados do canal em arquivo csv
@@ -148,15 +139,18 @@ async def collect():
 
             else:
                print(f'{Fore.RED}No content scraped.{Style.RESET_ALL}')
+            
 
             # Executa a funcao content_downloader para baixar arquivos postados
             try:
                 await content_downloader(channel_name, output_directory)
             except Exception as e:
                 print(f"[-] An error occurred: {Fore.RED}{e}{Style.RESET_ALL}")
-
+            
 
         except Exception as e:
             print(f"[-] An error occurred: {Fore.RED}{e}{Style.RESET_ALL}")
 
-        analyse(csv_filename,channel_name_filtered)
+        await analyse(csv_filename,channel_name_filtered)
+
+asyncio.run(collect())
