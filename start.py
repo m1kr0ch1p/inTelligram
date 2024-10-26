465334dc353173a93f1faf9a912dbf5960573924
Noob <53237138+m1kr0ch1p@users.noreply.github.com>
2024-10-25 21:59:26 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,81 +1,23 @@
-from colorama import init, Fore, Style
-from engine import collect
-import asyncio
+from colorama import Fore, Style
 
-# Funcao para criar/atualizar o arquivo details.py
-def update_details_file(api_id, api_hash, phone_number):
-    with open("details.py", "w") as file:
-        file.write(f'apiID = "{api_id}"\n')
-        file.write(f'apiHash = "{api_hash}"\n')
-        file.write(f'number = "{phone_number}"\n')
+# Inspired in https://docs.telethon.dev/en/stable/
 
-# Define os detalhes do usuario
-# OBS: A seguranca das credenciais pode ser otimizada se utilizar input para
-#      as variaveis.
+api_id = None
+api_hash = None
+phone_number = None
 
+# receive channels to collect and analyse
+def ignite():
+    global channel_list
+    channel_list = []
+    channel_file = 'channels.txt'
+    with open(channel_file, "r") as file:
+        for channel in file:
+            channel_list.append(channel.strip())
+    #print(channel_list)
 
-def ip_check():
-    import json
-    import requests
-
-    try:
-        webURL = 'http://ip-api.com/json/?fields=61439'
-        json_data = requests.get(webURL).json()
-
-        # Dumpando e Carregando o JSON
-        dataB = json.dumps(json_data)
-        data = json.loads(dataB)
-
-        # Extraindo informações
-        country_code = data['countryCode']
-        city = data['city']
-        query = data['query']
-
-        # Formato
-        formatted_info = f"{city}-{country_code} (IP Adress {query})"
-
-        # Imprimindo o resultado
-        print(f'[*] You are connected to Internet from {Fore.YELLOW}{formatted_info}{Style.RESET_ALL}\n')
-    except erro as e:
-        print(f'{Fore.RED}[-] {e}{Style.RESET_ALL}')
-
-#init(autoreset=True)
-
-while True:
-    try:
-        api_id = input("Please enter your API ID:\n")
-        print(f'You entered "{api_id}"')
-        confirmation = input('Is this correct? (y/n)')
-        if confirmation.lower() == 'y':
-            print('Updating...')
-            break
-    except:
-        continue
-
-while True:
-    try:
-        api_hash = input("Please enter your API Hash:\n")
-        print(f'You entered "{api_hash}"')
-        confirmation = input('Is this correct? (y/n)')
-        if confirmation.lower() == 'y':
-            print('Updating...')
-            break
-    except:
-        continue
-
-while True:
-    try:
-        phone_number = input("Please enter your phone number:\n")
-        print(f'You entered "{phone_number}"')
-        confirmation = input('Is this correct? (y/n)')
-        if confirmation.lower() == 'y':
-            print('Updating...')
-            break
-    except:
-        continue
-
-# Atualiza o arquivo details e executa a funca collect de engine
-
-update_details_file(api_id, api_hash, phone_number)
+# Calling local functions
 ip_check()
-asyncio.run(collect())
+ignite()
+# Calling data collector
+exec(open("engine.py").read())
