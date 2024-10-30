0f47742eb393c4a8ffdf7ce01171acd70da235ea
M1kr0ch1p <53237138+m1kr0ch1p@users.noreply.github.com>
2024-10-30 15:18:39 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import subprocess
+
+def download_corpora():
+    subprocess.call(['python3', '-m', 'textblob.download_corpora'])
+    subprocess.call(['python', '-m', 'spacy', 'download', 'pt_core_news_sm'])
+    subprocess.call(['python', '-m', 'spacy', 'download', 'en_core_web_sm'])
+    subprocess.call(['python', '-m', 'spacy', 'download', 'fr_core_news_sm'])
+    subprocess.call(['python', '-m', 'spacy', 'download', 'es_core_news_sm'])
+    subprocess.call(['python', '-m', 'spacy', 'download', 'ar_core_news_sm'])
+
+setup(
+    name='seu_projeto',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'jsonlib',
+        'requests',
+        'colorama',
+        'asyncio',
+        'pandas',
+        'telethon',
+        'plotly',
+        'matplotlib',
+        'numpy',
+        'wordcloud',
+        'os-sys',
+        'details',
+        'datetime',
+        'pillow',
+        'textblob==0.15.3',
+        'certifi',
+        'langdetect',
+        'csv',
+        'spacy'
+    ],
+    cmdclass={
+        'download_corpora': download_corpora,
+    },
+)
