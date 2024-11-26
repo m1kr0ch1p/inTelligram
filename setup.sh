a1e264f554dfbcbc8557f18cd55faf523dae5007
M1kr0ch1p <53237138+m1kr0ch1p@users.noreply.github.com>
2024-11-26 11:09:05 -0300

    Update and rename setup.py to setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 -m pip install colorama asyncio pandas telethon plotly matplotlib numpy wordcloud datetime pillow textblob==0.15.3 certifi langdetect spacy pytz nltk tzlocal corpora
+python -m spacy download en_core_web_sm
+python -m spacy download fr_core_news_sm
+python -m spacy download es_core_news_sm
+python -m spacy download pt_core_news_sm
+#python -m spacy download ar_core_news_sm
