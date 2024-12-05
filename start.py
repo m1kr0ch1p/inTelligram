559aa4b68ed853da827506281ef66f1792b8aee3
M1kr0ch1p <53237138+m1kr0ch1p@users.noreply.github.com>
2024-12-04 23:55:50 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,10 @@
 import asyncio
 from colorama import Fore, Style
+import sys
 
 # Banner
 def banner():
-    banner = f"""{Fore.RED}
+    banner = f"""{Fore.BLUE}
     ██╗███╗   ██╗████████╗███████╗██╗     ██╗     ██╗ ██████╗ ██████╗  █████╗ ███╗   ███╗
     ██║████╗  ██║╚══██╔══╝██╔════╝██║     ██║     ██║██╔════╝ ██╔══██╗██╔══██╗████╗ ████║
     ██║██╔██╗ ██║   ██║   █████╗  ██║     ██║     ██║██║  ███╗██████╔╝███████║██╔████╔██║
@@ -47,10 +48,27 @@ def ignite():
         for channel in file:
             channel_list.append(channel.strip())
 
+# create directories
+def create_dirs():
+    import os
+
+    # creates directories for each channel
+    for channel_name in channel_list:
+        # Gives channel name to directory
+        channel_name_filtered = channel_name.rsplit("/",1)[-1]
+        # Defines output for files store
+        output_directory = f'CaseFiles/{channel_name_filtered}'
+        # Verify or creates directory
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+
 # Calling local functions
 banner()
 ip_check()
 ignite()
+create_dirs()
 # Calling data collector
 exec(open("engines/usercollect.py").read())
 exec(open("engines/engine.py").read())
+exec(open("engines/analytics.py").read())
